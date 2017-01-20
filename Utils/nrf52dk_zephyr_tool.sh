f7aab7d92936d2c643b8a36731f438989dd48254
id101010 <aaron@duckpond.ch>
2017-01-20 22:53:58 +0100

    Modified nrf52dk_zephyr_tool

--- Utils/nrf52dk_zephyr_tool.sh
+++ Utils/nrf52dk_zephyr_tool.sh
@@ -2,15 +2,65 @@
 
 PROJPATH="$2" # path given by argument
 BINARYPATH="$PROJPATH/outdir/nrf52_pca10040/zephyr.hex"
-DEFAULT_MAKE="BOARD ?= nrf52_pca10040\nCONF_FILE ?= prj.conf\ninclude /opt/zephyr-project/Makefile.inc"
-DEFAULT_PRJ="CONFIG_SOC_FAMILY_NRF5=y\nCONFIG_SOC_SERIES_NRF52X=y\nCONFIG_BOARD_NRF52_PCA10040=y\n# enable uart driver\n#CONFIG_SERIAL=y\nCONFIG_UART_NRF5=y\n# enable console\nCONFIG_CONSOLE=y\nCONFIG_UART_CONSOLE=y\n# additional board options\nCONFIG_GPIO_AS_PINRESET=y\n# bluetooth\nCONFIG_BLUETOOTH=y\nCONFIG_BLUETOOTH_CONTROLLER=y\n# printk support for serial print\nCONFIG_PRINTK=y\n# nrf52 gpio support\nCONFIG_GPIO=y\n# enable support for DHT11 Moisture Sensor\nDHT_CHIP_DHT11=y\n"
 
-usage()
-{
+# Default files as base64
+FILE_DEFAULT_SUBMAKE="b2JqLXkgKz0gbWFpbi5vCg=="
+
+FILE_DEFAULT_MAKE="Qk9BUkQgPz0gbnJmNTJfcGNhMTAwNDAKQ09ORl9GSUxFID89IHByai5jb25mCgppbmNsdWRlICR7
+WkVQSFlSX0JBU0V9L01ha2VmaWxlLmluYwo="
+
+FILE_DEFAULT_PRJCONF="Q09ORklHX0FSTT15CkNPTkZJR19TT0NfRkFNSUxZX05SRjU9eQpDT05GSUdfU09DX1NFUklFU19O
+UkY1Mlg9eQpDT05GSUdfQk9BUkRfTlJGNTJfUENBMTAwNDA9eQoKIyBlbmFibGUgdWFydCBkcml2
+ZXIKI0NPTkZJR19TRVJJQUw9eQpDT05GSUdfVUFSVF9OUkY1PXkKCiMgZW5hYmxlIGNvbnNvbGUK
+Q09ORklHX0NPTlNPTEU9eQpDT05GSUdfVUFSVF9DT05TT0xFPXkKCiMgYWRkaXRpb25hbCBib2Fy
+ZCBvcHRpb25zCkNPTkZJR19HUElPX0FTX1BJTlJFU0VUPXkKCiMgYmx1ZXRvb3RoCkNPTkZJR19C
+TFVFVE9PVEg9eQpDT05GSUdfQkxVRVRPT1RIX0NPTlRST0xMRVI9eQpDT05GSUdfQkxVRVRPT1RI
+X0RFQlVHX0xPRz15CkNPTkZJR19CTFVFVE9PVEhfUEVSSVBIRVJBTD15CkNPTkZJR19CTFVFVE9P
+VEhfR0FUVF9EWU5BTUlDX0RCPXkKCiMgcHJpbnRrIHN1cHBvcnQgZm9yIHNlcmlhbCBwcmludApD
+T05GSUdfUFJJTlRLPXkKCiMgbnJmNTIgZ3BpbyBzdXBwb3J0CkNPTkZJR19HUElPPXkKCiMgZW5h
+YmxlIHN1cHBvcnQgZm9yIERIVDExIE1vaXN0dXJlIFNlbnNvcgpESFRfQ0hJUF9ESFQxMT15Cg=="
+
+FILE_DEFAULT_CPROG="LyoKICogQ29weXJpZ2h0IChjKSAyMDE2IEludGVsIENvcnBvcmF0aW9uCiAqCiAqIExpY2Vuc2Vk
+IHVuZGVyIHRoZSBBcGFjaGUgTGljZW5zZSwgVmVyc2lvbiAyLjAgKHRoZSAiTGljZW5zZSIpOwog
+KiB5b3UgbWF5IG5vdCB1c2UgdGhpcyBmaWxlIGV4Y2VwdCBpbiBjb21wbGlhbmNlIHdpdGggdGhl
+IExpY2Vuc2UuCiAqIFlvdSBtYXkgb2J0YWluIGEgY29weSBvZiB0aGUgTGljZW5zZSBhdAogKgog
+KiAgICAgaHR0cDovL3d3dy5hcGFjaGUub3JnL2xpY2Vuc2VzL0xJQ0VOU0UtMi4wCiAqCiAqIFVu
+bGVzcyByZXF1aXJlZCBieSBhcHBsaWNhYmxlIGxhdyBvciBhZ3JlZWQgdG8gaW4gd3JpdGluZywg
+c29mdHdhcmUKICogZGlzdHJpYnV0ZWQgdW5kZXIgdGhlIExpY2Vuc2UgaXMgZGlzdHJpYnV0ZWQg
+b24gYW4gIkFTIElTIiBCQVNJUywKICogV0lUSE9VVCBXQVJSQU5USUVTIE9SIENPTkRJVElPTlMg
+T0YgQU5ZIEtJTkQsIGVpdGhlciBleHByZXNzIG9yIGltcGxpZWQuCiAqIFNlZSB0aGUgTGljZW5z
+ZSBmb3IgdGhlIHNwZWNpZmljIGxhbmd1YWdlIGdvdmVybmluZyBwZXJtaXNzaW9ucyBhbmQKICog
+bGltaXRhdGlvbnMgdW5kZXIgdGhlIExpY2Vuc2UuCiAqLwoKI2luY2x1ZGUgPHplcGh5ci5oPgoj
+aW5jbHVkZSA8bWlzYy9wcmludGsuaD4KI2luY2x1ZGUgPGJvYXJkLmg+CiNpbmNsdWRlIDxkZXZp
+Y2UuaD4KI2luY2x1ZGUgPGdwaW8uaD4KCiNkZWZpbmUgREVCVUcKI2RlZmluZSBQT1JUICAgICAg
+ICAiR1BJT18wIgoKI2RlZmluZSBMRUQxICAgICAgICAgMTcKI2RlZmluZSBMRUQyICAgICAgICAg
+MTgKI2RlZmluZSBMRUQzICAgICAgICAgMTkKI2RlZmluZSBMRUQ0ICAgICAgICAgMjAKCiNkZWZp
+bmUgU0xFRVBfVElNRSAgMTAwCgovLyBEZWJ1ZyB1c2luZyBzZXJpYWwgdGVybWluYWwKdm9pZCBk
+ZWJ1Zyhjb25zdCBjaGFyICptZXNzYWdlKQp7CiNpZmRlZiBERUJVRwogICAgcHJpbnRrKCJbXGVb
+MTszMW1ERUJVR1xlWzBtXSAlc1xuIiwgbWVzc2FnZSk7CiNlbmRpZgp9Cgp2b2lkIG1haW4odm9p
+ZCkKewogICAgZGVidWcoIlN0YXJ0ZWQgcHJvZ3JhbS4iKTsKCiAgICBpbnQgY250ID0gMDsKICAg
+IAogICAgLy8gQ29uZmlndXJlIGdwaW8KICAgIHN0cnVjdCBkZXZpY2UgKmRldjsKCiAgICBkZXYg
+PSBkZXZpY2VfZ2V0X2JpbmRpbmcoUE9SVCk7CiAgICAKICAgIGdwaW9fcGluX2NvbmZpZ3VyZShk
+ZXYsIExFRDEsIEdQSU9fRElSX09VVCk7CiAgICBncGlvX3Bpbl9jb25maWd1cmUoZGV2LCBMRUQy
+LCBHUElPX0RJUl9PVVQpOwogICAgZ3Bpb19waW5fY29uZmlndXJlKGRldiwgTEVEMywgR1BJT19E
+SVJfT1VUKTsKICAgIGdwaW9fcGluX2NvbmZpZ3VyZShkZXYsIExFRDQsIEdQSU9fRElSX09VVCk7
+CgogICAgd2hpbGUgKDEpIHsKICAgICAgICBkZWJ1ZygiTWFpbiBjeWNsZS4iKTsKICAgICAgICBw
+cmludGsoIkN5Y2xlOiAlZFxuIiwgY250KTsKCiAgICAgICAgZ3Bpb19waW5fd3JpdGUoZGV2LCBM
+RUQxLCAoY250ICUgMikpOwogICAgICAgIGdwaW9fcGluX3dyaXRlKGRldiwgTEVEMiwgKGNudCAl
+IDMpKTsKICAgICAgICBncGlvX3Bpbl93cml0ZShkZXYsIExFRDMsIChjbnQgJSA0KSk7CiAgICAg
+ICAgZ3Bpb19waW5fd3JpdGUoZGV2LCBMRUQ0LCAoY250ICUgNSkpOwoKICAgICAgICBrX3NsZWVw
+KFNMRUVQX1RJTUUpOwogICAgICAgIGNudCsrOwogICAgfQp9Cg=="
+
+# Hack for declaration of tools on various systems
+GDBSERV="$(which JLinkGDBServer)"
+NRFJPRO="$(which nrfjprog)"
+
+usage(){
 cat << EOF
 Usage: $0 [options]
 
 This script handles the creation and compilation of zephyr applications for the nrf52dk.
+Make sure the Zephyr environment (zephyr-env.sh) is sourced! Otherwise the compilation will not work.
 
 OPTIONS:
    help                 Show this message
@@ -21,34 +71,42 @@ OPTIONS:
 EOF
 }
 
-start_debugger()
-{
+start_debugger(){
     JLinkGDBServer -Device nRF52832_xxAA -If SWD -Speed 4000 -Autoconnect 1
 }
 
-flash_nrf()
-{
+flash_nrf(){
     nrfjprog --eraseall -f nrf52
     nrfjprog --program $BINARYPATH -f nrf52
     nrfjprog --reset -f nrf52
 }
 
-build()
-{
+build(){
     make -C $PROJPATH BOARD=nrf52_pca10040
 }
 
-create_new()
-{
+create_new(){
+    # Create project structure and default files
     mkdir -p $PROJPATH/src
+    touch $PROJPATH/src/main.c
+    touch $PROJPATH/src/Makefile
     touch $PROJPATH/Makefile
     touch $PROJPATH/prj.conf
 
-    # Insert Makefile default
-    echo -e "$DEFAULT_MAKE" >> $PROJPATH/Makefile
+    # Insert content into the default file
+    echo "$(echo "$FILE_DEFAULT_MAKE"    | base64 -d)" >> $PROJPATH/Makefile
+    echo "$(echo "$FILE_DEFAULT_PRJCONF" | base64 -d)" >> $PROJPATH/prj.conf
+    echo "$(echo "$FILE_DEFAULT_SUBMAKE" | base64 -d)" >> $PROJPATH/src/Makefile
+    echo "$(echo "$FILE_DEFAULT_CPROG"   | base64 -d)" >> $PROJPATH/src/main.c
+}
 
-    # Insert prj.conf default for nrf52dk
-    echo -e "$DEFAULT_PRJ" >> $PROJPATH/prj.conf
+setup(){
+    echo "Not yet implemented. :("
+    # TODO
+    # download zephyr-sdk, extract to /opt
+    # download nrfjtools, extract to /opt
+    # download arm-none-eabi-tools, extract to /opt
+    #
 }
 
 case $1 in    
